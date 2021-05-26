09ae6196828e8e65981e6f1ea95bc7790e0a69a9
Cardoso-topdev <oscar@cardoso-devstar.com>
2021-05-26 12:29:43 -0500

    estilos css a nosotros

--- src/components/contenidoNosotros.js
+++ src/components/contenidoNosotros.js
@@ -2,6 +2,26 @@ import React from "react"
 import { graphql, useStaticQuery } from "gatsby";
 import Image from 'gatsby-image';
 import {css} from '@emotion/react';
+import styled from '@emotion/styled';
+
+
+const Contenido = styled.main`
+padding-top: 4rem;
+max-width: 1200px;
+width: 95%;
+margin: 0 auto;
+
+@media(min-width: 768px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+}
+
+p{
+    line-height: 2;
+    margin-top: 2rem;
+}
+`
 
 const ContenidoNosotros = () => {
   const informacion = useStaticQuery(graphql`
@@ -31,10 +51,10 @@ const ContenidoNosotros = () => {
       font-size: 4rem;
       margin-top: 4rem;`}
       >{titulo}</h2>
-      <div>
+      <Contenido>
       <p>{contenido}</p>
       <Image fluid={imagen.fluid} />
-      </div>
+      </Contenido>
     </>
   )
 }
--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ContenidoNosotros from '../components/contenidoNosotros';
-import Layout from '../components/layout';
+import React from "react"
+import ContenidoNosotros from "../components/contenidoNosotros"
+import Layout from "../components/layout"
 
 const Nosotros = () => {
-    return ( 
-
-        <Layout>
-            <ContenidoNosotros />
-        </Layout>
-     );
+  return (
+    <Layout>
+      <ContenidoNosotros />
+    </Layout>
+  )
 }
- 
-export default Nosotros;
+
+export default Nosotros
